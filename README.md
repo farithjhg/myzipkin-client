5533d8dbd10ba20b0bfa400009c1bb8e7c763d0c
fherasg <fheras.garcia@gmail.com>
2017-11-20 15:50:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# myzipkin-client
---
+++ src/test/java/com/example/myzipkinclient/MyzipkinClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.myzipkinclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyzipkinClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
